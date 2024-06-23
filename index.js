18003601921e0738919c39276b30e3990f07bfea
GiftedTerry <protechackers@gmail.com>
2024-06-23 01:52:34 +0100

    Vibez-MD😎

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 const db = require('./database');
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
 const bot = require(__dirname + '/lib/vibez');
 const { VERSION } = require(__dirname + '/config');
 
@@ -11,8 +6,7 @@ bot.on('message', (msg) => {
   const name = msg.author.name;
   const number = msg.author.phone;
   const message = msg.body;
-
-  db.run('INSERT INTO users (name, phone) VALUES (?, ?)', [name, number], function(err) {
+  db.run('INSERT INTO users (name, phone) VALUES (?, ?)', [name, number], (err) => {
     if (err) {
       console.error(err);
     } else {
@@ -24,7 +18,7 @@ bot.on('message', (msg) => {
 const start = async () => {
   try {
     await bot.init();
-    bot.logger.info('⏳ Database syncing!');
+    bot.logger.info('Database syncing!');
     await bot.DATABASE.sync();
     await bot.connect();
   } catch (error) {
@@ -37,11 +31,8 @@ start();
 
 app.get('/db', async (req, res) => {
   try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM test_table');
-    const results = { results: (result) ? result.rows : null };
-    res.render('pages/db', results);
-    client.release();
+    const results = await db.all('SELECT * FROM users');
+    res.render('pages/db', { results });
   } catch (err) {
     console.error(err);
     res.send("Error " + err);
